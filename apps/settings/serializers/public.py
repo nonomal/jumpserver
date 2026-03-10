d0a9d6525d5251ce67b597470ff0a4948bec970f
Bai <baijiangjie@gmail.com>
2026-03-10 11:29:11 +0800

    feat: add JDMC_ENABLED to settings API

--- apps/settings/serializers/public.py
+++ apps/settings/serializers/public.py
@@ -87,6 +87,8 @@ class PrivateSettingSerializer(PublicSettingSerializer):
     PRIVACY_MODE = serializers.BooleanField()
     CHANGE_SECRET_AFTER_SESSION_END = serializers.BooleanField()
 
+    JDMC_ENABLED = serializers.BooleanField()
+
 
 class ServerInfoSerializer(serializers.Serializer):
     CURRENT_TIME = serializers.DateTimeField()
